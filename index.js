35c40d7f2763276181c3e34dc7e0ab938ccbd79d
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-09-14 19:06:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -90,17 +90,6 @@ bot.on('raw', event => {
     }   
 });
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const ytdl = require('ytdl-core');
-const request = require('request');
-const fs = require('fs');
-const getYoutubeID = require('get-youtube-id');
-const fetchVideoInfo = require('youtube-info');
-
-const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const prefix = '$';
-
 
  bot.on('message', message => {
               if(!message.channel.guild) return;
