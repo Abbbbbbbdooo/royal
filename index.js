69c027115ace5eb3bf05652e7636c74c57d8d718
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-10-17 15:01:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -150,31 +150,6 @@ if (message.content.startsWith(prefix + 'setplay')) {
 });
 
 
-const arraySort = require('array-sort'),
-table = require('table');
-
-bot.on('message' , async (message) => {
-
-    if(message.content.startsWith(prefix + "top")) {
-
-  let invites = await message.guild.fetchInvites();
-
-    invites = invites.array();
-
-    arraySort(invites, 'uses', { reverse: true });
-
-    let possibleInvites = [['الدعوات', 'الاشخاص']];
-    invites.forEach(i => {
-      possibleInvites.push([i.inviter.username , i.uses]);
-    })
-    const embed = new Discord.RichEmbed()
-    .setColor(0x7289da)
-    .setTitle("دعوات السيرفر")
-    .addField(' المتصدرين' , `${table.table(possibleInvites)}`)
-
-    message.channel.send(embed)
-    }
-});
 
 bot.login(process.env.BOT_TOKEN)
 	
