75e2cfd342743c800b550a85833a482398b693ed
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-10-23 18:44:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -306,9 +306,8 @@ bot.on('message', message => {
 });
 
 bot.on('message',async message => {
-  if(message.author.bot || message.channel.type === 'dm') return;
-  let args = message.content.split(' ');
-  if(args[0] === `${prefix}bc`) {
+   let args = message.content.split(' ');
+   if(args[0] === `${prefix}bc`) {
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('- **أنت لا تملك الصلاحيات اللازمة لأستخدام هذا الأمر**');
     if(!args[1]) return message.channel.send('- **يجب عليك كتابة الرسالة بعد الأمر**');
   
