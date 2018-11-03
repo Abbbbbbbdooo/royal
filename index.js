113cf2a4fbe5a2c384c69b2c7cbf2a7bd0e7cc7f
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-11-03 16:39:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -300,6 +300,30 @@ bot.on("guildMemberAdd", m => {
     };
 });
 
+var prefix = "!";
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+  
+ 
+
+if (command == "e") {
+    let say = new Discord.RichEmbed()
+    .setDescription(args.join("  "))
+    .setColor("RANDOM")
+    message.channel.sendEmbed(say);
+    message.delete();
+  }
+
+
+});
+
 bot.login(process.env.BOT_TOKEN)
 	
 const prefix = '$';
