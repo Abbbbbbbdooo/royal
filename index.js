c8a973a59307a3f52b8cef1ca534208e8b0adbe1
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-11-15 12:49:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -118,23 +118,6 @@ message.guild.unban(ns);
 });
 
 
-const moment = require("moment")
-bot.on("guildMemberAdd", m => {
-        let room = m.guild.channels.find(a => a.name === 'fake'); //
-    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 2) {
-        m.ban() .then((
-            room.send(`**:no_entry: | ${m} Has been banned for: \`fake\`**`)
-        ));
-    };
-    function parseDate(str) {
-        var mdy = str.split('/');
-        return new Date(mdy[2], mdy[0]-1, mdy[1]);
-    };
-    
-    function datediff(first, second) {
-        return Math.round((second-first)/(1000*60*60*24));
-    };
-});
 
 
 bot.login(process.env.BOT_TOKEN)
