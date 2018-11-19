e74f1171f65b2f8e1367982bea954a1625bfeb2d
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-11-19 19:14:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
 
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
 
 
 
-client.on('message',async message => {
-  var prefix = '!';
-  if(message.author.bot) return;
-  if(message.channel.type === 'dm') return;
-  let args = message.content.split(' ');
-  let tag;
-  if(args[0] === `${prefix}discrim`) {
-    if(args[1]) {
-      let discrim = Array.from(args[1]);
-      if(isNaN(args[1])) return message.channel.send(`- \`${message.author.username}\`, يجب ان تتكون هذه الخانة من ارقام وليس احرف`);
-      if(discrim.length !== 4) return message.channel.send(`- \`${message.author.username}\`, يجب ان يكون التاق مكون من 4 ارقام`);
-
-      tag = discrim.map(r => r.toString()).join('');
-      console.log(tag);
-      if(hero.users.filter(f => f.discriminator === tag).size === 0) return message.channel.send(`- \`${message.author.username}\`, لا يوجد احد بهذا التاق`);
-      let iLD = new Discord.RichEmbed()
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setDescription(client.users.filter(f => f.discriminator === tag).map(r => r.username).slice(0, 10).join('\n'))
-      .setFooter('');
-      message.channel.send(iLD);
-    } else if(!args[1]) {
-      tag = message.author.discriminator;
-      if(client.users.filter(f => f.discriminator === tag).size === 0) return message.channel.send(`- \`${message.author.username}\`, لا يوجد احد بهذا التاق`);
-      let L4U = new Discord.RichEmbed()
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setDescription(client.users.filter(f => f.discriminator === tag).map(r => r.username).slice(0, 10).join('\n'))
-      .setFooter('');
-      message.channel.send(L4U);
+const hastebin = require('hastebin-gen');
+client.on('message', message => {
+var PREFIX = '!';
+    if(message.content.startsWith(PREFIX + 'discrim')) {
+            var args = message.content.split(' ').slice(1).join(' ');
+}
+      var array = [];
+      var i = 0;
+      if(args){
+client.users.filter(u => u.discriminator == args).map(u => {
+    if(i > 4){
+     return;
     }
-  }
+    i = i + 1;
+
+   array.push(`${u.tag}`);
+});
+}
+hastebin(`${array.slice(0, 30).join('\n')}`, 'txt').then(l => {
+    message.channel.send(`${l}`);
+}).catch(console.error);
+});
+client.on('message' , message => {
+var PREFIX = '!';
+if(message.content === `${PREFIX}discrim`) {
+                      let array = [];
+                      var i = 0;
+client.users.filter(u => u.discriminator == message.author.discriminator).map(u => {
+    if(i > 4){
+     return;
+    }
+    i = i + 1;
+   array.push(`${u.tag}`);
+});
+hastebin(`${array.slice(0, 30).join('\n')}`, 'txt').then(l => {
+    message.channel.send(`${l}`);
+}).catch(console.error);
+
+        }
 });
 
 
 
-bot.on('message', message => {
+client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
@@ -46,37 +55,37 @@ bot.on('message', message => {
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
-  bot.user.setActivity(argresult, {type: 'WATCHING'})
+  client.user.setActivity(argresult, {type: 'WATCHING'})
      console.log('test' + argresult);
     message.channel.sendMessage(`Watch Now: **${argresult}**`)
 } 
 
  
   if (message.content.startsWith(prefix + 'setlis')) {
-  bot.user.setActivity(argresult, {type: 'LISTENING'})
+  client.user.setActivity(argresult, {type: 'LISTENING'})
      console.log('test' + argresult);
     message.channel.sendMessage(`LISTENING Now: **${argresult}**`)
 } 
 
 
 if (message.content.startsWith(prefix + 'setname')) {
-  bot.user.setUsername(argresult).then
+  client.user.setUsername(argresult).then
       message.channel.sendMessage(`Username Changed To **${argresult}**`)
   return message.reply("You Can Only Chanage Name 2 Times Per Hour");
 } 
 
 if (message.content.startsWith(prefix + 'setavatar')) {
-  bot.user.setAvatar(argresult);
+  client.user.setAvatar(argresult);
    message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
 }
 
 if (message.content.startsWith(prefix + 'setstream')) {
-  bot.user.setGame(argresult, "https://www.twitch.tv/peery13");
+  client.user.setGame(argresult, "https://www.twitch.tv/peery13");
      console.log('test' + argresult);
     message.channel.sendMessage(`Streaming: **${argresult}**`)
 } 
 if (message.content.startsWith(prefix + 'setplay')) {
-  bot.user.setGame(argresult);
+  client.user.setGame(argresult);
      console.log('test' + argresult);
     message.channel.sendMessage(`Playing: **${argresult}**`)
 } 
@@ -84,7 +93,7 @@ if (message.content.startsWith(prefix + 'setplay')) {
 
 
 });
-bot.on('message',async message => {
+client.on('message',async message => {
 if(message.content === '$unbanall') {
 	        if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('❌|**\`ADMINISTRATOR\`لا توجد لديك صلاحية`**');
 message.guild.fetchBans().then(ba => {
@@ -97,6 +106,6 @@ message.guild.unban(ns);
 
 
 
-bot.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN)
 	
 const prefix = '$';
